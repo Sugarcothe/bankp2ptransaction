e1ab8ad356a4f3d66d8860600680b2be09d54660
Suagrcothe <ifeanyivalentine82@gmail.com>
2022-12-18 13:20:15 +0100

    walletId error

--- server/routes/transaction.js
+++ server/routes/transaction.js
@@ -2,8 +2,12 @@ const router = require("express").Router();
 const transactionController = require("../controllers/transaction");
 const auth = require("../middleware/verifyToken");
 
-router.post("/transfer", transactionController.transferFund);
-router.post("/completeTransfer", transactionController.completeTransaction);
+router.post("/transfer", auth, transactionController.transferFund);
+router.post(
+  "/completeTransfer",
+  auth,
+  transactionController.completeTransaction
+);
 router.get("/:walletId", auth, transactionController.history);
 
 module.exports = router;
