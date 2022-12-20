7dde754291a4ad70e087ef3bf0e705bde10a8d51
Suagrcothe <ifeanyivalentine82@gmail.com>
2022-12-20 18:31:36 +0100

    jwt error fix

--- server/middleware/verifyToken.js
+++ server/middleware/verifyToken.js
@@ -1,49 +1,55 @@
-import jwt from "jsonwebtoken"
-// const config = require("../config/index");
+import jwt from "jsonwebtoken";
 
-export const auth = (req, res, next) => {
+export const verifyUser = (req, res, next) => {
   const token = req.cookies.access_token;
+  
+  if (!token) 
+    return res.status(401).json({ message: "Access Denied!" });
 
-  if(!token) {
-    return next( res.status(401).json({
-        message: "You are not authenticated",
-      }))
+  try {
+    const verified = jwt.verify(token, process.env.JWT);
+    req.user = verified;
+
+    next();
+  } catch {
+    res.status(400).json({ message: "Invalid Token!" });
   }
+};
 
-  jwt.verify(token, process.env.JWT, (err, user) => {
-    if (err) {
-      return next(res.status(403).json("invalid token"));
-    } else {
-      req.user = user;
-      next();
-    }
-  });
-}
 
-export const verifyUser = (res, req) => {
-  verifyToken(req, res, () => {
-    if(req.user.id === req.params.id ){
-      next();
-    } else {
-      res.status(401).json({
-        message: "You are not authorized",
-      })
-    }
-  })
-}
 
-// module.exports = (req, res, next) => {
-//   const token = req.header("auth-token");
 
-//   if (!token) return res.status(401).json({ message: "Access Denied!" });
 
-//   try {
-//     const verified = jwt.verify(token, config.JWT_SECRET);
-//     req.user = verified;
 
-//     next();
-//   } catch {
-//     res.status(400).json({ message: "Invalid Token!" });
+
+
+// export const auth = (req, res, next) => {
+//   const token = req.cookies.access_token;
+
+//   if (!token) {
+//     return next(
+//       res.status(401).json({
+//         message: "You are not authenticated",
+//       })
+//     );
 //   }
+
+//   jwt.verify(token, process.env.JWT, (err, user) => {
+//     if (err) {
+//       return next(res.status(403).json("invalid token"));
+//     } else {
+//       req.user = user;
+//     }
+//   });
 // };
 
+// export const verifyUser = (res, req) => {
+//   auth(req, res, () => {
+//     if (req.user.id === req.params.id) {
+//     } else {
+//       res.status(403).json({
+//         message: "You are not authorized",
+//       });
+//     }
+//   });
+// };
