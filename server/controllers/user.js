85b862fad4e52b6db29d219b058d8cd0355c4933
Suagrcothe <ifeanyivalentine82@gmail.com>
2023-05-26 15:02:18 +0100

    controller updated with get user & users

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -93,3 +93,21 @@ export const login = async (req, res, next) => {
       user: { id: user._id, name: user.name },
     });
 };
+
+export const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(200).json(user);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getUsers = async (req, res, next) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    next(err);
+  }
+};
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { login, register } from "../controllers/user.js";
+import { getUser } from "../controllers/user.js";
+import { getUsers } from "../controllers/user.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/user/:id", getUser);
+router.get("/users", getUsers);
 
 export default router;
